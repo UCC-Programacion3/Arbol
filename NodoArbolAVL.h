ee1f6fc19ebc95cd5d0961049b65358126b98f6c
Matías Sebastián Murgui <msmurgui@github.com>
2021-10-28 17:27:44 -0300

    Arboles AVL agregados y main generado para probar eficiencia de ambos tipos de arboles al buscar un valor aleatorio en un rango de 5000 valores

--- NodoArbolAVL.h
+++ NodoArbolAVL.h
@@ -1,28 +1,31 @@
-#ifndef U05_ARBOL_ARBOL_NODOARBOL_H_
-#define U05_ARBOL_ARBOL_NODOARBOL_H_
+#ifndef U05_ARBOL_ARBOL_NODOARBOLAVL_H_
+#define U05_ARBOL_ARBOL_NODOARBOLAVL_H_
 
 #include <string.h>
 using namespace std;
 
 template <class T>
-class NodoArbol
+class NodoArbolAVL
 {
 private:
   T data;
-  NodoArbol *left, *right;
+  NodoArbolAVL *left, *right;
+  int height;
 
 public:
-  NodoArbol()
+  NodoArbolAVL()
   {
     left = nullptr;
     right = nullptr;
+    height=1;
   }
 
-  NodoArbol(T d)
+  NodoArbolAVL(T d)
   {
     data = d;
     left = nullptr;
     right = nullptr;
+    height = 1;
   }
 
   T getData() const
@@ -35,26 +38,34 @@ public:
     this->data = d;
   }
 
-  NodoArbol *getRight() const
+  NodoArbolAVL *getRight() const
   {
     return right;
   }
 
-  void setRight(NodoArbol *r)
+  void setRight(NodoArbolAVL *r)
   {
     this->right = r;
   }
-  NodoArbol *getLeft() const
+  NodoArbolAVL *getLeft() const
   {
     return left;
   }
 
-  void setLeft(NodoArbol *l)
+  void setLeft(NodoArbolAVL *l)
   {
     this->left = l;
   }
 
-   void print(bool esDerecho, string identacion) {
+  int getHeight(){
+    return height;
+  }
+
+  void setHeight(int h){
+    height = h;
+  }
+
+  void print(bool esDerecho, string identacion) {
     if (right != NULL) {
         right->print(true, identacion + (esDerecho ? "     " : "|    "));
     }
@@ -71,6 +82,6 @@ public:
     }
 }
 
-  };
+};
 
-#endif // U05_ARBOL_ARBOL_NODOARBOL_H_
+#endif // U05_ARBOL_ARBOL_NODOARBOLAVL_H_
